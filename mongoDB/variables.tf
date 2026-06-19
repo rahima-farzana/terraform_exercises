9ba0bc0049866c2b5acad5077d8bac38d93c5a62
scmlearningcentre <scmlearningcentre@gmail.com>
2026-06-19 11:49:39 +0530

    Create variables.tf

---
+++ mongoDB/variables.tf
@@ -0,0 +1,8 @@
+variable "project_id" {
+  description = "The ID of the project in MongoDB Atlas"
+  default     = "wezvatech"
+}
+
+variable "atlas_public_key" {}
+variable "atlas_private_key" {}
+variable "k8s_public_ip" {}
