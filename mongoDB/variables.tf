1f8837a7b6da2eb93e2d1ad2e4d6eccf84ce1284
scmlearningcentre <scmlearningcentre@gmail.com>
2026-06-20 19:19:59 +0530

    Update variables.tf

--- mongoDB/variables.tf
+++ mongoDB/variables.tf
@@ -3,6 +3,13 @@ variable "project_id" {
   default     = "wezvatech"
 }
 
+variable "allowed_ips" {
+  type        = list(string)
+  description = "List of public IPs to allow access"
+  default     = ["1.2.3.4", "5.6.7.8"] # Add your K8s public IP and local IP here
+}
+
 variable "atlas_public_key" {}
+
 variable "atlas_private_key" {}
-variable "k8s_public_ip" {}
+
